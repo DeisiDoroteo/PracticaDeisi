db98c69e69667fe0eee36583751c0f5a9d2960b9
DeisiDoroteo <20211321@uthh.edu.mx>
2024-10-30 18:46:05 -0600

    testsv3

--- testcafe.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  src: 'tests', // Directorio de tus archivos de prueba
-  browsers: ['edge:headless'], // Ejecutar pruebas en Edge headless
-  reporter: 'spec', // Reporte solo en consola
-  quarantineMode: true, // Reintenta pruebas fallidas
-  skipJsErrors: true, // Ignora errores de JavaScript en la página
-  pageLoadTimeout: 8000, // Tiempo máximo de carga de la página (en ms)
-};
